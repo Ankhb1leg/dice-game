d076def6500424f8964d342fa6e66893292665b1
Ankhaa <anhaa6599@gmail.com>
2023-06-19 09:22:38 +0800

    object ehnii huwisagch uusgesen

---
+++ app.js
@@ -0,0 +1,21 @@
+var activePlayer = 1;
+
+var scores = [0, 0];
+
+var roundScore = 0;
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+// <div class="player-score" id="score-0">43</div>
+// window.document.querySelector("#score-0").textContent = dice;
+
+// document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
+
+// ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼ ÑÑ…Ð»ÑÑ…ÑÐ´ Ð±ÑÐ»Ñ‚Ð³ÑÐµ
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
