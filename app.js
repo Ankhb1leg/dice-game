45f8b3ad4b9ff24eeeedc391f653f30c5da787c2
Ankhaa <anhaa6599@gmail.com>
2023-06-19 09:37:02 +0800

    shoo shiddeg bolgoson

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var scores = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.floor(Math.random() * 6) + 1;
+
 
 // <div class="player-score" id="score-0">43</div>
 // window.document.querySelector("#score-0").textContent = dice;
@@ -12,10 +12,20 @@ var dice = Math.floor(Math.random() * 6) + 1;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
 
 // ÐŸÑ€Ð¾Ð³Ñ€Ð°Ð¼ ÑÑ…Ð»ÑÑ…ÑÐ´ Ð±ÑÐ»Ñ‚Ð³ÑÐµ
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent= '0';
+document.getElementById("score-1").textContent= '0';
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    switch(diceNumber){
+       
+    }
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
+
