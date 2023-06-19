452da8834b0cbc6969b1d5600c4c68a9bdcce643
Ankhaa <anhaa6599@gmail.com>
2023-06-19 10:56:37 +0800

    winner and switch player

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var activePlayer = 1;
+var activePlayer = 0;
 
 var scores = [0, 0];
 
@@ -20,6 +20,8 @@ document.getElementById("current-1").textContent = '0';
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 
+
+//shoog shideh event handler 
 document.querySelector(".btn-roll").addEventListener('click', function(){
     var diceNumber = Math.floor(Math.random() * 6) + 1;
     switch(diceNumber){
@@ -27,5 +29,56 @@ document.querySelector(".btn-roll").addEventListener('click', function(){
     }
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
+
+    // toglogchiin eeljiin onoo uurchluh 
+
+    if(diceNumber!==1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+
+    }
+    else{
+      
+        
+        //ulaan tseg shiljuuleh
+        switchToNextPlayer();
+     
+
+
+    }
 });
 
+document.querySelector(".btn-hold").addEventListener('click', function(){
+    
+    scores[activePlayer] = scores[activePlayer]+roundScore;
+    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    if(scores[activePlayer]>=20){
+        document.getElementById("name-"+activePlayer).textContent = "WINNER!!!";
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    }
+    
+    else{
+        switchToNextPlayer();
+
+    }
+
+
+
+
+});
+
+
+
+function switchToNextPlayer(){
+    roundScore = 0;
+    document.getElementById("current-"+ activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer =1) : (activePlayer=0);
+
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    
+    diceDom.style.display = "none";
+}
